0a6a4fd186983f02447323f0943656d190290b16
Adam Tryba <adam.tryba93@gmail.com>
2019-08-29 19:30:24 +0200

    dokumentacja typów wyliczeniowych

--- Micromouse/enums.h
+++ Micromouse/enums.h
@@ -1,15 +1,19 @@
-#pragma once
+﻿#pragma once
 
+//! Typ wyliczeniowy.
+/*! Umozliwia identyfikacje globalnych kierunków. */
 enum DirectionEnum {
-	North = 0,
-	East = 1,
-	South = 2,
-	West = 3
+	North = 0, /*!< Północ - góra okna */ 
+	East = 1, /*!< Zachód - prawa strona okna */ 
+	South = 2, /*!< Południe - dół okna. */ 
+	West = 3 /*!< Zachód - lewa strona okna */ 
 };
 
+//! Typ wyliczeniowy.
+/*! Umozliwia identyfikacje boku wybranego obiektu. */
 enum SideEnum {
-	Left = 0,
-	Front = 1,
-	Right = 2,
-	Back = 3
+	Left = 0, /*!< Lewy bok/ krawędź. */ 
+	Front = 1, /*!< Przód. */ 
+	Right = 2, /*!< Prawy bok/ krawędź. */ 
+	Back = 3 /*!< Tył. */ 
 };
